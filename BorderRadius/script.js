c1590eac418fbe02c4adbfb0713ace6071de2709
StevenWrites <stevenjacobsen4444@gmail.com>
2022-08-23 13:05:57 -0400

    Initial Solution - BorderRadius
    The initial solution to my border radius problem. The ice cube texture is a stylistic choice which creates a sudo-animation when you hold down the up arrow starting at 0.

--- BorderRadius/script.js
+++ BorderRadius/script.js
@@ -1,2 +1,14 @@
-let borderRadius = 0;
+
+
+const input = document.querySelector('input');
+const log = document.getElementById('values');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  let inputValue = e.target.value;
+  console.log(inputValue);
+  document.getElementById("borderRadiusText").innerHTML = e.target.value;
+  document.getElementById('iceCubeArt').style.borderRadius = inputValue + 'px';
+}
 
